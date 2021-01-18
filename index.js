77216ffaec63fcc09c5346b604afc845f6896bbf
Dfisher <60874439+Drax-Fisher@users.noreply.github.com>
2021-01-18 23:39:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 var exper = "";
 var last_num = "";
+function check_p(){
+	var objDiv = document.querySelector("#p");
+		objDiv.scrollTop = objDiv.scrollHeight;
+}
 function exp(val){
 	exper += last_num
 	if (exper.length < 1) {
 		exper += 0;
 	}
 	exper += " " + val + " ";
-	document.querySelector('#display p').innerHTML = exper;
-	document.querySelector('#display input').value = "";
+	document.querySelector('#p').value = exper;
+	document.querySelector('#inp').value = "";
+	check_p();
 	last_num = "";
 
 }
 function int(vl){
-	last_num += vl;
-	document.querySelector('#display input').value = last_num;
+	if (last_num.length <= 10) {
+			last_num += vl;
+	}
+	document.querySelector('#inp').value = last_num;
 }
 function sub(){
 	exper += last_num;
 	var x = eval(exper);
 	last_num = x;
-	document.querySelector('#display input').value = x;
-	document.querySelector('#display p').innerHTML = exper + " =";
+	document.querySelector('#inp').value = x;
+	document.querySelector('#p').value = exper + " =";
+	check_p();
 	exper = "";
 }
 function clr(){
 	last_num = "";
 	exper = "";
-	document.querySelector('#display p').innerHTML = exper;
-	document.querySelector('#display input').value = "";
+	document.querySelector('#p').value = exper;
+	document.querySelector('#inp').value = "";
 }
 function change_sign(){
 	last_num = eval("-1*"+last_num);
-	document.querySelector('#display input').value = last_num;
+	document.querySelector('#inp').value = last_num;
+}
+function check(e, val){
+	last_num = document.querySelector("#inp").value;
+}
+function check_len(e, val){
+	var key = e.keyCode ? e.keyCode : e.which;
+	if ( key >= 48 && key <= 57 && last_num.length > 10) {
+		e.preventDefault();
+	}
 }
